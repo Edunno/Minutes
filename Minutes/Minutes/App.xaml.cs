1dc56e92b030fcb4fde7cf24fc8de36af9efb4fb
Edunno <tamericandream@hotmail.com>
2019-09-11 11:30:03 +0200

    Initial commit

---
+++ Minutes/Minutes/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Minutes.Data;
+
+namespace Minutes
+{
+    public partial class App : Application
+    {
+        public static INoteEntryStore Entries { get; set; }
+        public App()
+        {
+            InitializeComponent();
+            Entries = new MemoryEntryStore();
+            Entries.LoadMockData();
+            MainPage = new NavigationPage(new Minutes.MainPage())
+            {
+                BarBackgroundColor = Color.FromHex("#3498db"),
+                BarTextColor = Color.White
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+    
+}
---
+++ Minutes/Minutes/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Minutes.Data;
+
+namespace Minutes
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            entries.ItemsSource = await App.Entries.GetAllAsync();
+            entries.ItemTapped += OnItemTapped;
+            newEntry.Completed += OnAddNewEntry;
+        }
+        private async void OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            NoteEntry item = e.Item as NoteEntry;
+            await Navigation.PushAsync(new NoteEntryEditPage(item));
+        }
+        private async void OnAddNewEntry(object sender, EventArgs e)
+        {
+            string text = newEntry.Text;
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                NoteEntry item = new NoteEntry { Title = text };
+                await App.Entries.AddAsync(item);
+                await Navigation.PushAsync(new NoteEntryEditPage(item));
+                newEntry.Text = string.Empty;
+            }
+        }
+    }
+}
